58fb1a37c94cac0726d3f0f269a77e487017c08e
manikandan.v <manikandanv@8kmiles.com>
2022-12-09 15:50:11 +0530

    2nd commit

---
+++ src/test/java/com/okayjava/test/MayilappApplicationTests.java
@@ -0,0 +1,14 @@
+package com.okayjava.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayilappApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("tets started>>>>>>>>>>>>>>>>>");
+	}
+
+}
